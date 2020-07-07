6d0d1ea4bfb4441b00a1906d4fa3f85d4e9ffaf0
Edinson <sennin7k@gmail.com>
2020-07-06 19:20:57 -0500

    Se cambio a comillas sencillas

--- curso-profesional-de-JavaScript/assets/index.js
+++ curso-profesional-de-JavaScript/assets/index.js
@@ -1,9 +1,9 @@
-import MediaPlayer from "./MediaPlayer.js";
-import Autoplay from "./plugins/Autoplay.js";
+import MediaPlayer from './MediaPlayer.js';
+import Autoplay from './plugins/Autoplay.js';
 
-const video = document.querySelector("video");
-const button = document.querySelector("button");
-const btnMute = document.querySelector("#mute");
+const video = document.querySelector('video');
+const button = document.querySelector('button');
+const btnMute = document.querySelector('#mute');
 
 const player = new MediaPlayer({ el: video, plugins: [new Autoplay()] });
 
@@ -13,10 +13,10 @@ button.onclick = () => {
 
 btnMute.onclick = () => {
   if (video.muted) {
-    btnMute.textContent = "Mute";
+    btnMute.textContent = 'Mute';
     player.unmute();
   } else {
-    btnMute.textContent = "Unmute";
+    btnMute.textContent = 'Unmute';
     player.mute();
   }
   // video.muted ? player.unmute() : player.mute();
