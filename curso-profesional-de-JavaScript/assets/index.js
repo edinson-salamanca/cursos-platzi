2de322b299bea04957b6082b3747e473fe1889a6
Edinson <sennin7k@gmail.com>
2020-08-28 04:19:53 -0500

    Se emplementa IntersectionObserver

--- curso-profesional-de-JavaScript/assets/index.js
+++ curso-profesional-de-JavaScript/assets/index.js
@@ -1,11 +1,12 @@
 import MediaPlayer from './MediaPlayer.js';
 import Autoplay from './plugins/Autoplay.js';
+import AutoPause from './plugins/AutoPause.js';
 
 const video = document.querySelector('video');
 
 const player = new MediaPlayer({
   el: video,
-  plugins: [new Autoplay()],
+  plugins: [new Autoplay(), new AutoPause()],
 });
 
 const playButton = document.querySelector('#playButton');
