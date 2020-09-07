c60f2bf11ec5983839a18c06c3c1d2956d1f32df
Edinson <sennin7k@gmail.com>
2020-09-06 21:09:36 -0500

    Eventos en react

--- curso-practico-de-react-js/holamundo/src/components/Button.jsx
+++ curso-practico-de-react-js/holamundo/src/components/Button.jsx
@@ -1,13 +1,36 @@
 import React from 'react';
 
-const Button = (props) => {
-  const { text } = props;
-  return (
-    <div>
-      <button type='button'>{props.text}</button>
-      <button type='button'>{text}</button>
-    </div>
-  );
-};
+class Button extends React.Component {
+  state = {
+    count: 0,
+  };
+  // constructor(){
+  //   super();
+  //   this.handleClick = this.handleClick.bind(this);
+  // }
+
+  handleClick = () => {
+    this.setState({
+      count: this.state.count + 1,
+    });
+  };
+
+  // handleClick() {
+  //   this.setState({
+  //     count: this.state.count + 1,
+  //   });
+  // }
+  render() {
+    const { count } = this.state;
+    return (
+      <div>
+        <h1>Manzanas: {count}</h1>
+        <button type='button' onClick={this.handleClick}>
+          Click
+        </button>
+      </div>
+    );
+  }
+}
 
 export default Button;
