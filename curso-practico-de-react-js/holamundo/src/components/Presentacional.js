7eb17894851d0ed2ec2226f03624c7e6cc1a0259
Edinson <sennin7k@gmail.com>
2020-09-06 20:13:10 -0500

    Componente Stateless componentes Presentacional

---
+++ curso-practico-de-react-js/holamundo/src/components/Presentacional.js
@@ -0,0 +1,8 @@
+import React from 'react';
+/**
+ * Componentes que solo mostrar una parte espcial del html, estos 
+ * componente no tienen lógica ni ciclo de vida o estado
+ */
+const Presentacional = () => <h1>Hola mundo</h1>;
+
+export default Presentacional;
