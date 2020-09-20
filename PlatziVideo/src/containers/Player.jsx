ee6210e76fe13d79b9e54da15ccd6b1da0056901
Edinson <sennin7k@gmail.com>
2020-09-20 15:08:02 -0500

    Terminando de detallar el player

--- PlatziVideo/src/containers/Player.jsx
+++ PlatziVideo/src/containers/Player.jsx
@@ -1,17 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { connect } from 'react-redux';
+import { getVideoSource } from '../actions';
+import NotFound from './NotFound';
 import '../assets/styles/components/Player.scss';
 
-const Player = () => {
-  return (
+const Player = ({ history, match, playing, getVideoSource }) => {
+  const { id } = match.params;
+  const [loading, setLoading] = useState(true);
+  const hasPlaying = Object.keys(playing).length > 0;
+
+  useEffect(() => {
+    getVideoSource(id);
+    setLoading(false);
+  }, []);
+
+  if (loading) {
+    return <h2>Cargando...</h2>;
+  }
+  return hasPlaying ? (
     <div className='Player'>
       <video controls autoPlay>
-        <source src='' type='video/mp4' />
+        <source src={playing.source} type='video/mp4' />
       </video>
-      <div className='Player-black'>
-        <button type='button'>Regresar</button>
+      <div className='Player-back'>
+        <button type='button' onClick={() => history.goBack()}>
+          Regresar
+        </button>
       </div>
     </div>
+  ) : (
+    <NotFound />
   );
 };
 
-export default Player;
+const mapStateToProp = (state) => {
+  return {
+    playing: state.playing,
+  };
+};
+
+const mapDispatchToProp = {
+  getVideoSource,
+};
+export default connect(mapStateToProp, mapDispatchToProp)(Player);
