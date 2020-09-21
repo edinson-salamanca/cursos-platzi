2f77484cea38c89e64f1709d5e0e1da8e5b913a6
Edinson <sennin7k@gmail.com>
2020-09-20 20:00:16 -0500

    Se mejoro la UI del buscador como la de los botones

--- PlatziVideo/src/containers/Login.jsx
+++ PlatziVideo/src/containers/Login.jsx
@@ -78,7 +78,7 @@ const Login = (props) => {
           </section>
           <p className='login__container--register'>
             No tienes ninguna cuenta
-            <Link to='/register'>Regístrate</Link>
+            <Link to='/register'> Regístrate</Link>
           </p>
         </section>
       </section>
